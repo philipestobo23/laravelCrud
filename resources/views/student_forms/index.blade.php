b4aaf9b7da7fdaabc9691c9bc059ee4ccb2831bf
John Philip T. Estobo <105591828+philipestobo23@users.noreply.github.com>
2023-01-24 15:50:02 +0800

    mvc editing

---
+++ resources/views/student_forms/index.blade.php
@@ -0,0 +1,17 @@
+@extends('student_forms.layout')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+
+                        <h2>Student Form</h2>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
